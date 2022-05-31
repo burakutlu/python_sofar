6f829eaf365d7ade2cdf4394f6e1c91a41d97a60
Burak <64499281+burakutlu@users.noreply.github.com>
2022-05-31 23:04:27 +0300

    formatting fixed

--- pandas/squirrel/main.py
+++ pandas/squirrel/main.py
@@ -1,15 +1,15 @@
 import pandas
-import openpyxl
 
 data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
 
 output = {
-    "ID": [data["Unique Squirrel ID"].head(10)],
-    "Color": [data["Primary Fur Color"].head(10)]
+    "ID": [],
+    "Color": []
 }
-for i in range(1, 10):
-    output["ID"].append(data["Unique Squirrel ID"])
-    output["Color"].append(data["Primary Fur Color"])
+for i in range(10):
+    output["ID"].append(data["Unique Squirrel ID"].sample().values)
+    output["Color"].append(data["Primary Fur Color"].sample().values)
+
 
 output_data = pandas.DataFrame(output)
 output_data.to_excel("output.xlsx", index=False)
